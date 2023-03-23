a1ef0187dbb4e73f251b3b5eddea7d6546bfdcde
juanaugusto96 <121531914+juanaugusto96@users.noreply.github.com>
2023-03-22 21:59:16 -0300

    Update Card.js

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,9 +3,10 @@ import "./card.css";
 export const Card = () => {
     return (
         <div className="card">
-            <h1>sdasadsa</h1>
-            <p>dsaassa</p>
-            <p>sdasdsads</p>
+            <h1>tienda de celulares</h1>
+            <p>varias marcas</p>
+            <p>varios modelos
+       </p>
         </div>
     )
 }
